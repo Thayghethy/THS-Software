7ef5563ca6b2d31f50a435e6bc7a030a03335fa8
Thayghethy Souza dos Santos <101431239+Thayghethy@users.noreply.github.com>
2025-03-31 14:26:59 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function clickMenu() {
-    let menu = document.getElementById('nav');
-    let nav = document.getElementById('nav_links')
+    let nav = document.getElementById('nav_links');
     let burgerIcon = document.getElementById('burger');
     let closeIcon = document.getElementById('close');
-    
-    menu.classList.toggle('open');
+
     nav.classList.toggle('open');
 
-    if (menu.classList.contains('open')) {
+    if (nav.classList.contains('open')) {
         burgerIcon.style.display = 'none';
         closeIcon.style.display = 'block';
     } else {
